10f9c1a723da2226e704301e8e44591f00d5f5b6
Binayak Panda <pbinayak604@gmail.com>
2024-12-22 11:12:09 +0530

    .

---
+++ matrix/search_in_matrix-II.cpp
@@ -0,0 +1,16 @@
+bool matSearch(vector<vector<int>> &mat, int x)
+{
+    int n = mat.size();
+    int m = mat[0].size();
+    int row = 0, col = m - 1;
+    while (row < n && col >= 0)
+    {
+        if (mat[row][col] == x)
+            return true;
+        else if (mat[row][col] < x)
+            row++;
+        else
+            col--;
+    }
+    return false;
+}
